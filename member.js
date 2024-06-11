945c02ffef9c928b261bb9e53ad90b06fc0250e5
Ruben Castillo Torres <96348790+Rubenhct25@users.noreply.github.com>
2024-06-11 17:07:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  var skills = [
+    {
+      "name": "JavaScript",
+      "level": "Intermediate"
+    },
+    {
+      "name": "HTML",
+      "level": "Intermediate"
+    },
+    {
+      "name": "CSS",
+      "level": "Intermediate"
+    }
+  ];
+
+  return skills;
+}
